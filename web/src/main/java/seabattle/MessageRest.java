525e891eeb0fe6e8498245cef18787b67fa915e4
Denis <denchik.anofriev@gmail.com>
2021-09-09 12:45:24 +0300

    Some problems

--- web/src/main/java/seabattle/MessageRest.java
+++ web/src/main/java/seabattle/MessageRest.java
@@ -5,12 +5,16 @@ package seabattle;/*
 
 
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.*;
 import seabattle.database.AuthorizationCrud;
 import seabattle.database.authorizationJpa;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 import java.util.*;
 
 
@@ -63,6 +67,27 @@ public class MessageRest implements MessageController {
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World!!");
         return model;
+    }
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .httpBasic().and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            // @formatter:on
+        }
     }
 }
 
