fec0432c5c44f08af1196a570d1a360b7b8ed739
Denis <denchik.anofriev@gmail.com>
2021-09-14 12:43:43 +0300

    spring-sequrity dependency fix

--- web/src/main/java/seabattle/MessageRest.java
+++ web/src/main/java/seabattle/MessageRest.java
@@ -8,11 +8,11 @@ package seabattle;/*
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.*;
 import seabattle.database.AuthorizationCrud;
 import seabattle.database.authorizationJpa;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.*;
@@ -73,20 +73,16 @@ public class MessageRest implements MessageController {
         return user;
     }
     @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
             http
-                    .httpBasic().and()
+                    .httpBasic()
+                    .and()
                     .authorizeRequests()
                     .antMatchers("/index.html", "/", "/home", "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .csrf()
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-
+                    .anyRequest().authenticated();
         }
     }
 }
